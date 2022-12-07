13e2f04d8056acb86e6c196c557520b0d912e8bd
ArtroxGabriel <artroxgabriel@gmail.com>
2022-12-07 13:49:19 -0300

    puts errado

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "./logtree.h"
 #include "./fila_fifo.h"
@@ -7,9 +8,9 @@
 int main(int narg, char * argv[]) {
     Escalonador e;
     if(narg == 1){
-        puts("Nenhum arquivo encontrado");
+        puts("Nenhum parametro encontrado.");
         return EXIT_FAILURE;
     }
-    e_rodar(&e, argv[1], argv[2]);
+    e_rodar(&e, argv[1], "saida-0000.txt");
     return EXIT_SUCCESS;
 }
